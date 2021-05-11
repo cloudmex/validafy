297a00f5a4a3212224d6a216e7d895c376cd2158
Chidoman <exmy.so@gmail.com>
2021-05-11 16:30:25 -0600

    Add & Doc: documented landing and added the required fields at profile body: -- the profile table returns the txhash,ipfshash,time and the token id of each document -- added documentation at landing .js

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -9,7 +9,7 @@ import Sidebar from "../components/Sidebar.js";
 console.log(window.ethereum);
 export default function Profile() {
   const [Documents, setDocuments] = useState([]);
-  const [info, setInfo] = useState([]);
+  const [Profile, setProfile] = useState([]);
   useEffect(() => {
     (async () => {
       window.history.pushState(null, document.title, window.location.href);
@@ -17,14 +17,15 @@ export default function Profile() {
         window.history.pushState(null, document.title, window.location.href);
       });
 
-      window.ethereum._metamask.isUnlocked().then(function(value){
-        if(value){       console.log("Abierto")
-      }
-      else{ console.log("Cerrado");
-      window.location.href=("/")
-    }
+      window.ethereum._metamask.isUnlocked().then(function(value) {
+        if (value) {
+          console.log("Abierto");
+        } else {
+          console.log("Cerrado");
+          window.location.href = "/";
+        }
       });
-     
+
       if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
 
@@ -45,35 +46,72 @@ export default function Profile() {
           return;
         }
         //instantiate the contract object
-        setInfo({address: useraccounts});
+        setProfile({ address: useraccounts });
         let contract = new window.web3.eth.Contract(
           ValidafySM.abi,
           tokenNetworkData.address
         );
+        //obtenemos sus documentos
+        let tokensarr = await contract.methods
+          .documentsOF(useraccounts[0])
+          .call();
 
-        //obtenemos sus tokens
-        let tokensarr = await contract.methods.tokensOf(useraccounts[0]).call();
-        //sacamos los hashes
-
-        console.log(tokensarr.tokens);
+        //arreglo con todos los documentos
         let documents = [];
-        if (tokensarr.tokens) {
-          for (let tokenid of tokensarr.tokens) {
-            documents.push(await contract.methods.tokenURI(tokenid).call());
+        //si no esta vacio agregamos todos los documentos al arreglo documents
+        if (tokensarr) {
+          for (let doc of tokensarr) {
+            //hash se refiere al ipfshash
+            documents.push({ hash: doc.hash, tokenid: doc.tokenid });
           }
         }
+        //obtenemos todas las transacciones de esos tokens
+        let eventos = await contract.getPastEvents("Transfer", {
+          filter: { tokenId: documents.map((x) => x.tokenid) },
+          fromBlock: 0,
+          toBlock: "latest",
+        });
+
+        //le agregamos a documents el time y el txhash
+        for (let i = 0; i < eventos.length; i++) {
+          let txhash = await window.ethereum.request({
+            method: "eth_getBlockByHash",
+            params: [eventos[i].blockHash, false],
+          });
+          documents[i] = {
+            ...documents[i],
+            time: new Date(
+              parseInt(txhash.timestamp, 16) * 1000
+            ).toLocaleString(),
+            txhash: eventos[i].transactionHash,
+          };
+        }
+
+        //le agregamos a documents el time y el txhash
 
         console.log(documents);
         setDocuments(documents);
-        
       }
 
       console.log(window.ethereum);
     })();
   }, []);
+  /*
   var today = new Date(),
-  date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()+" \nHOURS: "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()+"";
-
+    date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate() +
+      " \nHOURS: " +
+      today.getHours() +
+      ":" +
+      today.getMinutes() +
+      ":" +
+      today.getSeconds() +
+      "";
+*/
   return (
     <>
       {" "}
@@ -85,7 +123,7 @@ export default function Profile() {
               className="absolute top-0 w-full h-full bg-center bg-cover"
               style={{
                 backgroundImage:
-                  "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')",
+                  "url('https://images.unsplash.com/photo-1566132127697-4524fea60007?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80')",
               }}
             >
               <span
@@ -118,13 +156,13 @@ export default function Profile() {
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64  -mr-30">
                 <div className="px-6">
                   <div className="flex flex-wrap justify-end">
-                   <div className="w-full lg:w-6/12 px-4 lg:order-1 lg:text-center lg:self-end">
-                      <div className="   ">
-                      <div className="mr-4 p-3 text-center">
-                          <span className="text-xs  block   tracking-wide text-gray-700">
-                          {info.address}
+                    <div className="w-full lg:w-3/12 px-4 lg:order-2 flex-1 ml-4">
+                      <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                        <div className="mr-4 p-3 text-center">
+                          <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                            {Profile.address}
                           </span>
-                          <span className="text-sm text-gray-500">ADDRESS</span>
+                          <span className="text-sm text-gray-500">Address</span>
                         </div>
                       </div>
                     </div>
@@ -132,12 +170,12 @@ export default function Profile() {
                       <div className="flex justify-center py-4 lg:pt-4 pt-8">
                         <div className="mr-4 p-3 text-center">
                           <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                             {Documents.length}
+                            {Documents.length}
                           </span>
-                          <span className="text-sm text-gray-500">Documentos</span>
+                          <span className="text-sm text-gray-500">
+                            Documentos
+                          </span>
                         </div>
-                        
-                         
                       </div>
                     </div>
                     <div className="w-full lg:w-3/12 px-4 lg:order-3 flex justify-center">
@@ -145,12 +183,11 @@ export default function Profile() {
                         <img
                           alt="..."
                           src={require("../assets/img/metamask.png")}
-                          className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                          className=" rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
                           style={{ maxWidth: "150px" }}
                         />
                       </div>
                     </div>
-                    
                   </div>
                   <div className="  text-center   mt-12">
                     <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
@@ -160,7 +197,7 @@ export default function Profile() {
                     <table className="table-fixed text-center overflow-y-auto w-full  mr-4 mb-6  bg-gray-200 shadow-lg bg-pink">
                       <thead>
                         <tr>
-                        <th className="bg-blue-300 border text-right px-8 py-4">
+                          <th className="bg-blue-300 border text-right px-8 py-4">
                             TokenID
                           </th>
                           <th className="bg-blue-100 border text-right px-8 py-4">
@@ -171,32 +208,37 @@ export default function Profile() {
                             IpfsHash
                           </th>
                           <th className="bg-blue-100 border text-right px-8 py-4">
-                          TimeStamp
+                            TimeStamp
                           </th>
                         </tr>
                       </thead>
                       <tbody>
                         {Documents.map((doc, i) => (
-                          <tr key={i} >
+                          <tr key={i}>
                             <td className="border text-xs px-8 py-4">
-                              <a>{i}</a>
+                              <a>{doc.tokenid}</a>
                             </td>
                             <td className="border text-xs px-8 py-4">
-                            <a>{i}</a>
+                              <a
+                                href={
+                                  "https://testnet.bscscan.com/tx/" + doc.txhash
+                                }
+                              >
+                                {doc.txhash}
+                              </a>
                             </td>
                             <td className="border text-xs px-8 py-4">
                               {" "}
                               <a
                                 className="a-link"
-                                href={`https://ipfs.infura.io/ipfs/${doc}`}
+                                href={`https://ipfs.infura.io/ipfs/${doc.hash}`}
                               >
-                                {`${doc}`}{" "}
+                                {`${doc.hash}`}{" "}
                               </a>
                             </td>
                             <td className="border text-xs px-8 py-4">
-                            <a>{date}</a>
+                              <p>{doc.time}</p>
                             </td>
-                             
                           </tr>
                         ))}
                       </tbody>
@@ -217,7 +259,7 @@ export default function Profile() {
                       href="https://cloudmex.io/"
                       className="text-gray-600 hover:text-gray-900"
                     >
-                      CloudMex Analytics  
+                      CloudMex Analytics
                     </a>
                     .
                   </div>
