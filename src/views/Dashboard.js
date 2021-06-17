70655c6b22be66ece3964ef4ff99ce343e43c1ec
javier <jn168546@gmail.com>
2021-06-16 22:31:08 -0600

    e with '#' will be ignored, and an empty message aborts the commit.

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -213,7 +213,7 @@ console.log(tokenNetworkData)
         if (file === undefined) {
           window.location.reload();
         }
-
+        
         if (!event.target.files) {
           throw "no agrego ningun archivo";
         }
@@ -664,18 +664,18 @@ console.log(tokenNetworkData)
                                 <div>
                                   {!showHideCharge ? (
                                     <label className="w-full flex flex-col items-center px-4 py-6 bg-white rounded-lg  tracking-wide uppercase  cursor-pointew-full flex flex-col items-center px-4   mtbg-white rounded-lg  tracking-wide uppercase  cursor-pointer ">
-                                      <svg
+                                      {initialBc.showImg && <svg
                                         className="w-8 h-8 text-pink-600"
                                         fill="currentColor"
                                         xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20"
                                       >
                                         <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                                      </svg>
+                                      </svg>}
 
-                                      <span className="mt-2 text-base leading-normal">
+                                      {initialBc.showImg && <span className="mt-2 text-base leading-normal">
                                         Selecciona un archivo
-                                      </span>
+                                      </span>}
 
                                       <input
                                         type="file"
@@ -683,14 +683,7 @@ console.log(tokenNetworkData)
                                         accept=".pdf"
                                         onChange={Validar}
                                         required
-                                        onClick={() => {
-                                          setInitialBc({
-                                            ...initialBc,
-                                            Validado: "",
-                                            showImg: !initialBc.showImg,
-                                            Validar,
-                                          });
-                                        }}
+                                        onClick={() => { setInitialBc({ ...initialBc, Validado:"", showImg: true, Validar }); }}
                                       />
                                     </label>
                                   ) : (
