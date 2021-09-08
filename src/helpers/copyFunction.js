966b7be6b1f15471f9ca197132302ef54f51a521
Willia Miranda <williammiranda1502@gmail.com>
2021-09-07 18:08:29 -0600

    Se arreglaron los problemas al cargar los Docs body: Se identifico y arrglo el problema con las carga de los documentos en src/views/Profile.js

---
+++ src/helpers/copyFunction.js
@@ -0,0 +1,10 @@
+ const v = (text) => {
+    console.log('text', text)
+    var textField = document.createElement('textarea')
+    textField.innerText = text
+    document.head.appendChild(textField)
+    textField.select()
+    document.execCommand('copy')
+    textField.remove()
+  }
+  v("euuu");
